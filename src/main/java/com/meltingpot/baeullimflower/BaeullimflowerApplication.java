b605e5fa9c2f5c9e6e817fcd39d6e7f4c0fa47cb
Lee Hannah <mohan0226@naver.com>
2024-07-04 01:37:36 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/meltingpot/baeullimflower/BaeullimflowerApplication.java
@@ -0,0 +1,13 @@
+package com.meltingpot.baeullimflower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeullimflowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeullimflowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meltingpot/baeullimflower/BaeullimflowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meltingpot.baeullimflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeullimflowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
