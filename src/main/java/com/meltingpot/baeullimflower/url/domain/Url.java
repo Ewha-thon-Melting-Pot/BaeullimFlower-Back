0e6242b81e0efcb021b24f37f3d127cfa7446b1d
Lee Hannah <mohan0226@naver.com>
2024-07-07 22:38:47 +0900

    feat: Url Entity 생성

---
+++ src/main/java/com/meltingpot/baeullimflower/url/domain/Url.java
@@ -0,0 +1,24 @@
+package com.meltingpot.baeullimflower.url.domain;
+
+import com.meltingpot.baeullimflower.post.domain.Post;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Url {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "url_id", updatable = false)
+    private Long urlId;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne
+    @Column(name = "post_id", updatable = false)
+    private Post post;
+}
