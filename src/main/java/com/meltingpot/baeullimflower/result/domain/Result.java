b255d794abfec7e9ec5fffebd0bef336722b010f
Lee Hannah <mohan0226@naver.com>
2024-07-07 22:33:51 +0900

    feat: Result Entity 생성

---
+++ src/main/java/com/meltingpot/baeullimflower/result/domain/Result.java
@@ -0,0 +1,31 @@
+package com.meltingpot.baeullimflower.result.domain;
+
+
+import com.meltingpot.baeullimflower.global.entity.BaseTimeEntity;
+import com.meltingpot.baeullimflower.member.domain.Member;
+import com.meltingpot.baeullimflower.post.domain.Post;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Result extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "result_id", updatable = false)
+    private Long resultId;
+
+    @Column(nullable = false)
+    private String content;
+
+    @OneToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", updatable = false)
+    private Member member;
+}
