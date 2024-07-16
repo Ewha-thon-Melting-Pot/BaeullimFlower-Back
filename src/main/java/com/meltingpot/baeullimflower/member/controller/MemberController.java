9e912a7b064dcabcad8e2c9864fd95fe6d032958
Lee Hannah <mohan0226@naver.com>
2024-07-16 17:17:03 +0900

    feat: 로그아웃 메서드 추가

--- src/main/java/com/meltingpot/baeullimflower/member/controller/MemberController.java
+++ src/main/java/com/meltingpot/baeullimflower/member/controller/MemberController.java
@@ -2,9 +2,14 @@ package com.meltingpot.baeullimflower.member.controller;
 
 import com.meltingpot.baeullimflower.member.dto.SignupRequestDto;
 import com.meltingpot.baeullimflower.member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,5 +22,11 @@ public class MemberController {
     public ResponseEntity<String> signup(@RequestBody SignupRequestDto requestDto){
         Long memberId =  memberService.save(requestDto);
         return ResponseEntity.ok("회원가입이 완료되었습니다. 유저 아이디: "+memberId);
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return ResponseEntity.ok("로그아웃 되었습니다.");
     }
 }
